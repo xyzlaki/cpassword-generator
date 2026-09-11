ff9c9261484e2854032c356ec052cd152127bf9e
Loyalty10K <lazarlukic011@gmail.com>
2026-09-11 23:58:24 +0200

    Enhance password_generator.c with comments
    Added comments to clarify the purpose of the seed and character array.

--- password_generator.c
+++ password_generator.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 int main() {int length;
-    srand(time(NULL));
-    char allchars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    srand(time(NULL)); //our seed for generating the random password, not the best method but works for simple password generators like these
+    char allchars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"; //all posible chars, you can define more
     int numchars = sizeof(allchars) -1;
     printf("Enter the desired length for your password: ");
-    scanf("%d", &length);
+    scanf("%d", &length); 
     printf("Your password is: ");
     for (int i = 0; i < length; i++) {
+        //making the password in this for loop
         int index = rand() % numchars;
         printf("%c", allchars[index]);}
 return 0;}
